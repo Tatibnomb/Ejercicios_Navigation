4bac731409d0239c40815be388ebf19489442344
Tatibnomb <49379010@est.ort.edu.ar>
2025-09-01 10:45:05 -0300

    Ejercicios Navigation con 1 minion

---
+++ Assets/AgenteScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgenteScript : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Transform targetTR;
+    [SerializeField] Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = targetTR.position;
+        anim.SetFloat("Speed", agent.velocity.magnitude);
+    }
+}
